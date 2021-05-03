ac9980f0e3e2f784cc05cf1ad0899cba9b1c705a
Subham-RKB <subhamchapagain45@gmail.com>
2021-05-03 14:55:18 +0545

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:solohackathon/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,28 +32,63 @@ final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class _MySignInPage extends State<MySignInPage> {
+  Color primaryColor = Color(0xff18203d);
+  Color secondaryColor = Color(0xff232c51);
+  Color logoGreen = Color(0xff25bcbb);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Sign Page'),
-      ),
-      body: Center(
+      backgroundColor: primaryColor,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 40),
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                child: Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Text(
-                    'Sign_in_with_Google',
-                    style: TextStyle(color: Colors.black, fontSize: 24),
-                  ),
-                ),
-                onPressed: _signInWithGoogle,
-              )
-            ]),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //We take the image from the assets
+            /*Image.asset(
+                    'assets/splash-image.png',
+                    height: 250,
+                  ),*/
+            SizedBox(
+              height: 20,
+            ),
+            //Texts and Styling of them
+            Text(
+              'Welcome to App Maker!',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'A platform to make a brand new app for you small start up or organization',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            //Our MaterialButton which when pressed will take us to a new screen named as
+            //LoginScreen
+            MaterialButton(
+              elevation: 0,
+              height: 50,
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => LoginScreen()));
+              },
+              color: logoGreen,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Let\'s Go...',
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                  Icon(Icons.arrow_forward_ios)
+                ],
+              ),
+              textColor: Colors.white,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -63,6 +99,5 @@ _signInWithGoogle() async {
   final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
   final AuthCredential credential = GoogleAuthProvider.credential(
       idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-  final UserCredential user =
-      (await firebaseAuth.signInWithCredential(credential));
+  final User user = (await firebaseAuth.signInWithCredential(credential)).user;
 }
